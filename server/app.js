8f8d2b5c61d38af342b5e68df4a7c179ca5d0d54
kpbhumika <kpbhumika@gmail.com>
2024-10-09 21:20:54 -0400

    intermediate commit. Running app with session

--- server/app.js
+++ server/app.js
@@ -2,9 +2,13 @@
 require("dotenv").config();
 const express = require('express');
 const cors = require('cors'); // Import CORS to handle cross-origin requests
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
 
-const dataRouter = require('./routes/data'); // Import the data routes
-const indexRouter = require('./routes/index'); // Import the data routes
+const pool = require('./db'); // Import the database connection
+const dataRouter = require('./routes/data');
+const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
 
 
 const PORT = process.env.PORT || 9000;
@@ -12,10 +16,24 @@ const app = express();
 
 // Middleware
 app.use(cors()); // Enable CORS
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    store: new pgSession(
+        {
+            pool: pool,
+            tableName: 'session'
+            // TODO: convert to migration file. Added the table from here: https://github.com/voxpelli/node-connect-pg-simple/blob/HEAD/table.sql
+        }
+    )
+})); //Add session middleware
 app.use(express.json()); // Enable JSON body parsing
 
 // Use the data routes
 app.use('/', indexRouter);
+app.use('/', authRouter); // Prefix all routes from data.js with /api
 app.use('/api', dataRouter); // Prefix all routes from data.js with /api
 
 // Start the server
