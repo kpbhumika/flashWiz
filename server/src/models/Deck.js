c0c3eca83f9165d5aa3c7597e61f8412762b2e5e
kpbhumika <kpbhumika@gmail.com>
2024-11-07 13:54:10 -0500

    updated deck model to add category

--- server/src/models/Deck.js
+++ server/src/models/Deck.js
@@ -8,22 +8,24 @@ class Deck extends Model {
   static get jsonSchema() {
     return {
       type: "object",
-      required: ["title", "userId", "isPublic"],
+      required: ["title", "userId", "isPublic", "categoryId"], // Add `categoryId` as required
       properties: {
         id: { type: "integer" },
         title: { type: "string", minLength: 1 },
         description: { type: "string" },
         userId: { type: "integer" },
+        categoryId: { type: "integer" }, // Foreign key to category
         isPublic: {
           type: "boolean",
-          default: false, // Private by default
+          default: false,
         },
       },
     };
   }
 
   static get relationMappings() {
-    const { User, Flashcard } = require("./index.js");
+    const { User, Flashcard, Category } = require("./index.js");
+
     return {
       user: {
         relation: Model.BelongsToOneRelation,
@@ -39,6 +41,14 @@ class Deck extends Model {
         join: {
           from: "decks.id",
           to: "flashcards.deckId",
+        },
+      },
+      category: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: Category,
+        join: {
+          from: "decks.categoryId",
+          to: "categories.id",
         },
       },
     };
