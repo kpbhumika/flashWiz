7cab554a6e4edd9cecb1aa42fd39b6402f80de11
kpbhumika <kpbhumika@gmail.com>
2024-11-13 18:54:51 -0500

    refactored navbar

--- client/src/layout/Navigation.js
+++ client/src/layout/Navigation.js
@@ -1,14 +1,13 @@
 
 import { LoginSection } from "./LoginSection";
-import CategorySearch from "../components/CategorySearch";
-import { BrowserRouter as Router } from "react-router-dom";
+import "../HeroSection.css"
 
 const Navigation = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+      <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#433878' }}>
+        <div className="container-fluid" >
+          <a className="navbar-brand" href="/" style={{ color: 'white' }} >
             FlashWiz
           </a>
           <button
@@ -24,11 +23,6 @@ const Navigation = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              {/* <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li> */}
               <li className="nav-item">
                 <a className="nav-link" href="/decks">
                   My Decks
@@ -44,43 +38,6 @@ const Navigation = () => {
                   Study
                 </a>
               </li>
-              {/* <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled" href="/">
-                  Disabled
-                </a>
-              </li> */}
             </ul>
 
             <LoginSection />
