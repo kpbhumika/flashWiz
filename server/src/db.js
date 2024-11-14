9c7d5c36c18bf2c480de82a0270d3cad40ec86bc
kpbhumika <kpbhumika@gmail.com>
2024-11-13 22:27:04 -0500

    refactor db

--- server/src/db.js
+++ server/src/db.js
@@ -4,11 +4,11 @@ const knex = require("knex");
 
 // Create a PostgreSQL connection pool
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "flashwiz",
-  password: "postgres",
-  port: 5432,
+  user: process.env["POSTGRES_USER"],
+  host: process.env["POSTGRES_HOST"],
+  database: process.env["POSTGRES_DB"],
+  password: process.env["POSTGRES_PASSWORD"],
+  port: process.env["POSTGRES_PORT"],
 });
 
 // Create a Knex instance
